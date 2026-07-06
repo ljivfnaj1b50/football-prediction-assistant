26e63e551837d264a5771e473b6ffb86186a4e69
ljivfnaj1b50 <armless.engine-0h@icloud.com>
2026-07-06 22:45:56 +0800

    更新部署脚本同步数据目录

--- deploy/update.sh
+++ deploy/update.sh
@@ -10,12 +10,14 @@ cd $APP_DIR
 git pull origin main
 
 mkdir -p $WEB_DIR
+mkdir -p $WEB_DIR/data
 cp index.html $WEB_DIR/index.html
 cp styles.css $WEB_DIR/styles.css
 cp app.js $WEB_DIR/app.js
 cp model.js $WEB_DIR/model.js
+cp data/matches.json $WEB_DIR/data/matches.json
 
 nginx -t
 systemctl restart nginx
 
-echo "UPDATE_OK"
+echo "UPDATE_OK_WITH_DATA"

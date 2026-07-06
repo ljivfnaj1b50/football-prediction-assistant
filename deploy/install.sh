c53557c2b0057acbb07320857b16ce04fc8d7db1
ljivfnaj1b50 <armless.engine-0h@icloud.com>
2026-07-06 22:23:19 +0800

    新增腾讯云服务器安装脚本

---
+++ deploy/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -e
+
+PROJECT=football-prediction-assistant
+SITE=jingxi-football
+REPO=https://github.com/ljivfnaj1b50/football-prediction-assistant.git
+
+apt update
+apt install -y git nginx
+
+mkdir -p /opt
+cd /opt
+
+if [ -d /opt/$PROJECT/.git ]; then
+  cd /opt/$PROJECT
+  git pull origin main
+else
+  git clone $REPO /opt/$PROJECT
+fi
+
+mkdir -p /var/www/$SITE
+cp /opt/$PROJECT/index.html /var/www/$SITE/index.html
+cp /opt/$PROJECT/styles.css /var/www/$SITE/styles.css
+cp /opt/$PROJECT/app.js /var/www/$SITE/app.js
+cp /opt/$PROJECT/model.js /var/www/$SITE/model.js
+
+cat > /etc/nginx/conf.d/$SITE.conf <<'EOF'
+server {
+    listen 80;
+    server_name _;
+    root /var/www/jingxi-football;
+    index index.html;
+
+    location / {
+        try_files $uri $uri/ /index.html;
+    }
+}
+EOF
+
+nginx -t
+systemctl enable nginx
+systemctl restart nginx
+
+echo "OK: http://124.223.187.223"
